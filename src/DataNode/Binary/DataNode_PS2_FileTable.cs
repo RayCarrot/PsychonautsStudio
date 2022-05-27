84eb5516ffcb6aadce3e3d8dcd955482d4969446
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-05-27 20:53:15 +0200

    Support PAL PS2 packs

--- src/DataNode/Binary/DataNode_PS2_FileTable.cs
+++ src/DataNode/Binary/DataNode_PS2_FileTable.cs
@@ -18,12 +18,32 @@ public class DataNode_PS2_FileTable : BinaryDataNode<PS2_FileTable>
 
     public override IEnumerable<DataNode> CreateChildren(FileContext fileContext)
     {
-        Dictionary<uint, string> fileTable = PS2_FileNames.FileTable.ToDictionary(x => PS2_FileEntry.GetFilePathHash(x));
+        Dictionary<uint, string> fileTable = PS2_FileNames.FileTable.ToDictionary(PS2_FileEntry.GetFilePathHash);
+
+        // The PAL version has a second pack
+        long masterPackLength = fileContext.FileStream.Length;
+        bool hasSecondPack = SerializableObject.Files.Any(x => x.AbsoluteOffset >= masterPackLength);
+
+        Stream[] pakStreams;
+        
+        if (hasSecondPack)
+        {
+            const string key = "PS2_PAK2";
+
+            if (!fileContext.HasDependency(key))
+                fileContext.AddDependency(key, Path.Combine(Path.GetDirectoryName(fileContext.FilePath), "RESOURC2.PAK")); // Hard-code for now
+
+            pakStreams = new[] { fileContext.FileStream, fileContext.GetDependency(key).FileStream };
+        }
+        else
+        {
+            pakStreams = new[] { fileContext.FileStream };
+        }
 
         yield return DataNode_Folder.FromUntypedFiles(
             files: SerializableObject.Files.Where(x => x.FileSize >= 0),
             fileContext: fileContext,
             getFilePathFunc: x => fileTable.TryGetValue(x.FilePathHash, out string? v) ? v : $"{x.FilePathHash:X8}",
-            getFileStreamFunc: (file, _) => new MemoryStream(file.ReadFile(fileContext.FileStream)));
+            getFileStreamFunc: (file, _) => new MemoryStream(file.ReadFile(pakStreams)));
     }
 }
--- src/IO/FileContext.cs
+++ src/IO/FileContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using PsychoPortal;
 
@@ -6,5 +7,23 @@ namespace PsychonautsTools;
 
 public record FileContext(string FilePath, Stream FileStream, PsychonautsSettings Settings, IBinarySerializerLogger? Logger) : IDisposable
 {
-    public void Dispose() => FileStream.Dispose();
+    private readonly Dictionary<string, FileContext> _dependencies = new();
+
+    public bool HasDependency(string key) => _dependencies.ContainsKey(key);
+    public void AddDependency(string key, string filePath)
+    {
+        if (!File.Exists(filePath))
+            throw new FileNotFoundException($"File {filePath} does not exist", filePath);
+
+        _dependencies.Add(key, new FileContext(filePath, File.OpenRead(filePath), Settings, Logger));
+    }
+    public FileContext GetDependency(string key) => _dependencies[key];
+
+    public void Dispose()
+    {
+        FileStream.Dispose();
+
+        foreach (FileContext dep in _dependencies.Values)
+            dep.Dispose();
+    }
 }
